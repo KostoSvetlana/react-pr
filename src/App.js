6b3bf9808e7d5aaf2d3863622326a494e8bb37ce
Svetlana <Kostosv585@yandex.ru>
2022-03-23 14:38:13 +0300

    hv1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Message } from "./components/Message/Message";
+
+const lastName = "Sokol";
 
 function App() {
-  return (
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Message name="Alexander" lastName={lastName} />
+        <Message name="Sveta" age={17} />
+         
     </div>
   );
 }
+
+//<Message name={name} asd={15 + 2} doSmth={foo} bold={true} />
+//<Message name="Other name" asd={20 + 10} doSmth={foo} />
+
+
+// export class AppClass extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
