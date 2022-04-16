df357248d7f3e876080055c8bd58ee76c73441bb
Svetlana <Kostosv585@yandex.ru>
2022-04-16 08:12:28 +0300

    hw-7

--- src/store/messages/actions.js
+++ src/store/messages/actions.js
@@ -1,3 +1,5 @@
+import { author } from "../../utils/const";
+
 export const ADD_MESSAGE = "MESSAGES::ADD_MESSAGE";
 export const INIT_MESSAGES_FOR_CHAT = "MESSAGES:: INIT_MESSAGES_FOR_CHAT";
 export const CLEAR_MESSAGES_FOR_CHAT = "MESSAGES:: CLEAR_MESSAGES_FOR_CHAT";
@@ -20,3 +22,26 @@ export const clearMessages = (chatId) =>({
     payload: chatId,
 
 });
+
+let timeout;
+
+export const addMessageWithReply = (newMsg, chatId) => (dispatch, getState) => {
+    dispatch(addMessage(newMsg, chatId));
+  
+    if (newMsg?.author === author.me) {
+        clearTimeout(timeout);
+
+      timeout = setTimeout(() => {
+        dispatch(
+          addMessage(
+            {
+              author: author.bot,
+              text: "hello friend",
+              id: `msg-${Date.now()}`,
+            },
+            chatId
+          )
+        );
+      }, 1000);
+    }
+  };
