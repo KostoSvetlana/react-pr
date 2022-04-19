5df66499837bd5b57bdb2d501fc0cb86c648b5d6
Svetlana <Kostosv585@yandex.ru>
2022-04-19 12:31:40 +0300

    hw-8

--- src/utils/const.js
+++ src/utils/const.js
@@ -2,3 +2,13 @@ export const author = {
     me: "me",
     bot: "bot",
   };
+
+  
+export const apiUrl = "https://api.spaceflightnewsapi.net/v3/artiles";
+
+export const FETCH_STATUSES = {
+  IDLE: "idle",
+  REQUEST: "request",
+  FAILURE: "failure",
+  SUCCESS: "success",
+};
